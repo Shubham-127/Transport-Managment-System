795399cd87b6c8642e8240ab09e5dc03b25eb020
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-18 11:53:03 +0530

    CustomUserDetailsService updated

--- src/main/java/com/example/UserCRUD/security/CustomUserDetailsService.java
+++ src/main/java/com/example/UserCRUD/security/CustomUserDetailsService.java
@@ -1,5 +1,38 @@
 package com.example.UserCRUD.security;
 
+import com.example.UserCRUD.model.User;
+import com.example.UserCRUD.repository.userRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public class CustomUserDetailsService {
+import java.util.List;
+
+import static java.util.Arrays.stream;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService{
+
+    private final userRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found with email:" + email));
+
+        List<SimpleGrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .toList();
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
 }
