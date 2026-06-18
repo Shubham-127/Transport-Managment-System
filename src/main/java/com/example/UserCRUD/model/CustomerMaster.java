2400b2bdddfdd2ca5c4c600cbbaba6cb974d2915
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-18 15:38:04 +0530

    created the entity for the CustomerMaster

---
+++ src/main/java/com/example/UserCRUD/model/CustomerMaster.java
@@ -0,0 +1,64 @@
+package com.example.UserCRUD.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table(name = "CustomerMaster")
+@Getter@Setter@NoArgsConstructor@AllArgsConstructor
+public class CustomerMaster {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long customerId;
+
+    @Column(nullable = false)
+    private String customerName;
+
+    @Column(nullable = false)
+    private String getNumber;
+
+    @Column(nullable = false)
+    private String panNumber;
+
+    @Column(nullable = false)
+    private String contactPerson;
+
+    @Column(nullable = false)
+    private String mobile;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String pincode;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String active;
+
+    @Column(nullable = false)
+    private String remarks;
+
+    @Column(nullable = false)
+    private String salesOrder;
+
+
+
+}
