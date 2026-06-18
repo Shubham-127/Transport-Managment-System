0dafe12349bd8c91fd1bcdc500260e134fac060d
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-18 15:29:05 +0530

    create the dto for the Login response

--- src/main/java/com/example/UserCRUD/dto/response/LoginResponsedto.java
+++ src/main/java/com/example/UserCRUD/dto/response/LoginResponsedto.java
@@ -1,11 +1,8 @@
 package com.example.UserCRUD.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter@Setter@NoArgsConstructor@AllArgsConstructor
+@Getter@Setter@NoArgsConstructor@AllArgsConstructor@Builder
 public class LoginResponsedto {
     private String Token;
     private String email;
