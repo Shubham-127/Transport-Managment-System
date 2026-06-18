46b277b420a0152aa68b1e632673d19c985c9d17
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-18 11:52:37 +0530

    LoginResponsedto updated

---
+++ src/main/java/com/example/UserCRUD/dto/response/LoginResponsedto.java
@@ -0,0 +1,12 @@
+package com.example.UserCRUD.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter@Setter@NoArgsConstructor@AllArgsConstructor
+public class LoginResponsedto {
+    private String Token;
+    private String email;
+}
