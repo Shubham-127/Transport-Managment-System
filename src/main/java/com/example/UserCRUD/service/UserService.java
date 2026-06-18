d2d9b3ec948fa599e1439e0d6360f865336edb19
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-18 15:31:22 +0530

    added the LoginResponedto

--- src/main/java/com/example/UserCRUD/service/UserService.java
+++ src/main/java/com/example/UserCRUD/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.UserCRUD.service;
 
 
+import com.example.UserCRUD.dto.request.LoginRequestdto;
 import com.example.UserCRUD.dto.request.UpdateRequestdto;
 import com.example.UserCRUD.dto.request.createRequestdto;
+import com.example.UserCRUD.dto.response.LoginResponsedto;
 import com.example.UserCRUD.dto.response.responsedto;
 
 import java.util.List;
@@ -12,6 +14,7 @@ public interface UserService {
     List<responsedto> getAllUsers();
     responsedto getUserById(Long id);
     responsedto updateUser (Long id, UpdateRequestdto request);
+    LoginResponsedto login(LoginRequestdto request);
 
      void deleteUser(Long id);
 }
