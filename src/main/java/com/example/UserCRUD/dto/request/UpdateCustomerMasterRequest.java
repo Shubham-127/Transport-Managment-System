689ff1e47158d2aafcc8869640188e80334f55f7
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-18 16:32:38 +0530

    created the UpdateCustomerMasterRequest DTO

---
+++ src/main/java/com/example/UserCRUD/dto/request/UpdateCustomerMasterRequest.java
@@ -0,0 +1,25 @@
+package com.example.UserCRUD.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter@Setter@AllArgsConstructor@NoArgsConstructor
+public class UpdateCustomerMasterRequest {
+    private Long customerId;
+    private String customerName;
+    private String getNumber;
+    private String panNumber;
+    private String contactPerson;
+    private String mobile;
+    private String email;
+    private String address;
+    private String city;
+    private String state;
+    private String pincode;
+    private String country;
+    private String active;
+    private String remarks;
+    private String salesOrder;
+}
