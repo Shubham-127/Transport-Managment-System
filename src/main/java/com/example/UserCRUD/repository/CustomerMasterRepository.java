e2bd97506e2fa171aefd9ff39ad087f2b5d7f596
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-18 15:52:12 +0530

    Created the CustomerMasterDeatils

---
+++ src/main/java/com/example/UserCRUD/repository/CustomerMasterRepository.java
@@ -0,0 +1,15 @@
+package com.example.UserCRUD.repository;
+
+
+import com.example.UserCRUD.model.CustomerMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerMasterRepository extends JpaRepository<CustomerMaster, Long> {
+
+    Optional<CustomerMaster> findByName(String customerName);
+    boolean existsByName(String customerName);
+}
---
+++ src/main/java/com/example/UserCRUD/service/CustomeMasterService.java
@@ -0,0 +1,5 @@
+package com.example.UserCRUD.service;
+
+
+public class CustomeMasterService {
+}
