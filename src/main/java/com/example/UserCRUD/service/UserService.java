535b6d452a8e1d0967961d604a367ed9eab0cc49
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-19 12:05:49 +0530

    update

--- src/main/java/com/example/UserCRUD/service/UserService.java
+++ src/main/java/com/example/UserCRUD/service/UserService.java
@@ -2,18 +2,18 @@ package com.example.UserCRUD.service;
 
 
 import com.example.UserCRUD.dto.request.LoginRequestdto;
-import com.example.UserCRUD.dto.request.UpdateRequestdto;
-import com.example.UserCRUD.dto.request.createRequestdto;
+import com.example.UserCRUD.dto.request.UpdateUserRequestdto;
+import com.example.UserCRUD.dto.request.CreateUserRequestdto;
 import com.example.UserCRUD.dto.response.LoginResponsedto;
 import com.example.UserCRUD.dto.response.responsedto;
 
 import java.util.List;
 
 public interface UserService {
-    responsedto createUser (createRequestdto request);
+    responsedto createUser (CreateUserRequestdto request);
     List<responsedto> getAllUsers();
     responsedto getUserById(Long id);
-    responsedto updateUser (Long id, UpdateRequestdto request);
+    responsedto updateUser (Long id, UpdateUserRequestdto request);
     LoginResponsedto login(LoginRequestdto request);
 
      void deleteUser(Long id);
