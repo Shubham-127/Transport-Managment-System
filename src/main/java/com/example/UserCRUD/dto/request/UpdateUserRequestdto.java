49aaec82380f5bf74d278012d332e797b382e79b
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-19 12:05:11 +0530

    update the name of the dto

--- src/main/java/com/example/UserCRUD/dto/request/UpdateUserRequestdto.java
+++ src/main/java/com/example/UserCRUD/dto/request/UpdateUserRequestdto.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter@Setter@NoArgsConstructor@AllArgsConstructor
-public class UpdateRequestdto {
+public class UpdateUserRequestdto {
     private String name;
     private String email;
 
