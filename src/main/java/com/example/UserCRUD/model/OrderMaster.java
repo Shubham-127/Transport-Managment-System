4ccf32e450c11f60cf4cf67a579ae0bf43553b2b
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-19 16:18:39 +0530

    created the orderMaster

---
+++ src/main/java/com/example/UserCRUD/model/OrderMaster.java
@@ -0,0 +1,60 @@
+package com.example.UserCRUD.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "OrderMaster")
+public class OrderMaster {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long orderNumber;
+
+    @Column(nullable = false)
+    private String orderType;
+
+    @Column(nullable = false)
+    private String company;
+
+    @Column(nullable = false)
+    private String customerNumber;
+
+    @Column(nullable = false)
+    private String orderDate;
+
+    @Column(nullable = false)
+    private String requestedDate;
+
+    @Column(nullable = false)
+    private String branchPlant;
+
+    @Column(nullable = false)
+    private Long shipToCustomerNumber;
+
+    @Column(nullable = false)
+    private String shipToCustomerName;
+
+    @Column(nullable = false)
+    private String shipToAddress1;
+
+    @Column(nullable = false)
+    private String shipToCity;
+    @Column(nullable = false)
+    private String shipToPinCode;
+    @Column(nullable = false)
+    private String unitWeight;
+    @Column(nullable = false)
+    private String weightUnit;
+    @Column(nullable = false)
+    private String unitVolume;
+    @Column(nullable = false)
+    private String volumeUnit;
+    @Column(nullable = false)
+    private String status;
+    @Column(nullable = false)
+    private String currencyCode;
+
+
+}
