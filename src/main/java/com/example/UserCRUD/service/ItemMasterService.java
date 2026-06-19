e5a14e9c1e598e0d54d942031bc936d3eebcfa4c
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-19 16:16:31 +0530

    created the service for the Itemmaster

--- src/main/java/com/example/UserCRUD/service/ItemMasterService.java
+++ src/main/java/com/example/UserCRUD/service/ItemMasterService.java
@@ -1,5 +1,17 @@
 package com.example.UserCRUD.service;
 
 
+import com.example.UserCRUD.dto.request.CreateItemMasterRequest;
+import com.example.UserCRUD.dto.request.UpdateItemMasterRequest;
+import com.example.UserCRUD.dto.response.ItemMasterResponse;
+
+import java.util.List;
+
 public interface ItemMasterService {
+
+    ItemMasterResponse createItem(CreateItemMasterRequest request);
+    List<ItemMasterResponse> getAllItems();
+    ItemMasterResponse getItemById(Long id);
+    ItemMasterResponse updateItem(Long id, UpdateItemMasterRequest request);
+    void deleteItem(Long id);
 }
