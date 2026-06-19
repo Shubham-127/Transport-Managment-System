b92e11d6de7d2e9f07df15909835730a11626a38
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-19 16:13:59 +0530

    CreateItemMasterRequest created

--- src/main/java/com/example/UserCRUD/dto/request/CreateItemMasterRequest.java
+++ src/main/java/com/example/UserCRUD/dto/request/CreateItemMasterRequest.java
@@ -1,5 +1,16 @@
 package com.example.UserCRUD.dto.request;
 
+import lombok.*;
 
+@Getter@Setter@NoArgsConstructor@AllArgsConstructor@Builder
 public class CreateItemMasterRequest {
+
+    private String itemNumber;
+    private Long shortItemNo;
+    private String description;
+    private String description2;
+    private String thirdItemNumber;
+    private String lineType;
+    private String stockingType;
+
 }
