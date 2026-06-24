a27354eac6c62c29b01009d6f89bc36e5d86fee6
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-06-24 15:54:42 +0530

    RouteMasterRequest

---
+++ src/main/java/com/example/UserCRUD/dto/request/Update/UpdateRouteMasterRequest.java
@@ -0,0 +1,15 @@
+package com.example.UserCRUD.dto.request.Update;
+
+
+import lombok.*;
+
+@Getter
+@Setter @NoArgsConstructor @AllArgsConstructor
+public class UpdateRouteMasterRequest {
+
+    private String routeCode;
+    private String sourceLocation;
+    private String destinationLocation;
+    private Double distance;
+    private String estimatedTime;
+}
