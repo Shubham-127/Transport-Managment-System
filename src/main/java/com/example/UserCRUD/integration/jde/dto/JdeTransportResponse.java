34bb59a4ae364f8ad87614216a23a3fdddcae238
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-07-01 22:04:56 +0530

    Jde Transport response

---
+++ src/main/java/com/example/UserCRUD/integration/jde/dto/JdeTransportResponse.java
@@ -0,0 +1,24 @@
+package com.example.UserCRUD.integration.jde.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JdeTransportResponse {
+
+    @JsonProperty("V560999P Count")
+    private int count;
+
+    @JsonProperty("DREQ_Transport_Master")
+    private List<JdeTransportMaster> transportMasters;
+
+    private String jde__status;
+}
