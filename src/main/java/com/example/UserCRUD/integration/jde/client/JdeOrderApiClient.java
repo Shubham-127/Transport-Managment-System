6c6145011b08e6b126faa5d6fdc05f14c2d752e5
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-07-03 14:44:21 +0530

    Jde order update

--- src/main/java/com/example/UserCRUD/integration/jde/client/JdeOrderApiClient.java
+++ src/main/java/com/example/UserCRUD/integration/jde/client/JdeOrderApiClient.java
@@ -6,33 +6,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientRequestException;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-@Component@RequiredArgsConstructor@Slf4j
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class JdeOrderApiClient {
-    @Qualifier("JdeWebClient")
+
+    @Qualifier("jdeWebClient")
     private final WebClient jdeWebClient;
 
-    public JdeOrderResponse getOrder(){
-        log.info("Calling JDE order API");
-
-        try{
-            JdeOrderResponse response = jdeWebClient.get()
-                    .uri("/ORCHSales_Order")
-                    .retrieve()
-                    .bodyToMono(JdeOrderResponse.class)
-                    .block();
-            log.info("Successfully received response from JDE");
-            return response;
-        }catch (WebClientResponseException e){
-            log.error("JDE returned HTTP{}:{}", e.getStatusCode(), e.getResponseBodyAsString());
-
-            throw e;
-        }catch (Exception e){
-            log.error("Failed to call JDE order API", e);
-            throw e;
-        }
+    public JdeOrderResponse getOrders() {
+        log.info("Calling JDE API to fetch orders");
+        return jdeWebClient.get()
+                .uri("/ORCHSales_Order")
+                .retrieve()
+                .bodyToMono(JdeOrderResponse.class)
+                .block();
     }
-
 }
