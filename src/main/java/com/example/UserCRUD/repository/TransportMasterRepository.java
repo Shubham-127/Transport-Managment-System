b7aa0557081c4eb4ecacb76450cf7da4b1dd0369
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-07-03 14:44:45 +0530

    Jde TransportMasterRepository

--- src/main/java/com/example/UserCRUD/repository/TransportMasterRepository.java
+++ src/main/java/com/example/UserCRUD/repository/TransportMasterRepository.java
@@ -4,8 +4,11 @@ import com.example.UserCRUD.model.TransportMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TransportMasterRepository extends JpaRepository<TransportMaster, Long> {
 
     boolean existsByGstNumber(String gstNumber);
+    Optional<TransportMaster> findByAddressNumber(Integer addressNumber);
 }
