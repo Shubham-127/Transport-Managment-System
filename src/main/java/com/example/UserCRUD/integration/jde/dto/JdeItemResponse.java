4aaa90faba13af6ca2b1efc4bb5d6ae4e4d1803a
Shubham Srivastava <shubham.srivastava1207@gmail.com>
2026-07-06 17:43:58 +0530

    jde item response

---
+++ src/main/java/com/example/UserCRUD/integration/jde/dto/JdeItemResponse.java
@@ -0,0 +1,22 @@
+package com.example.UserCRUD.integration.jde.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter@Setter@NoArgsConstructor@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JdeItemResponse {
+    @JsonProperty("F4101 Count")
+    private int count;
+
+    @JsonProperty("DREQ_Item_Master")
+    private List<JdeItem> itemMaster;
+
+    private String jde__status;
+}
